7cb3d6d3e2e377bfa2acba1b15b4f9300d524f12
LumeUSD <eth.bitcoin05@gmail.com>
2025-04-26 11:19:49 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-LumE USD (LUSD)
-Стабильный токен на блокчейне Solana
-О токене
-Тикер: LUSD
-Адрес: HjHgtmRntsg3wU4aED7PfQ6oFq4XbmMrLjmRAkG5LumE
-Десятичные знаки: 8
-Блокчейн: Solana
-Telegram: t.me/LumeUSD
-GitHub: github.com/LumeUSD
-© 2025 LumE USD. Все права защищены.
+import { lumeToken } from './tokenlist';
 
+export const tokenList = [
+  lumeToken,
+];
+
+export default tokenList;
