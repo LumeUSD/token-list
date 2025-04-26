e5ec14cb74c6bb34d942bcd15c14dd48c1fa454e
LumeUSD <eth.bitcoin05@gmail.com>
2025-04-26 11:18:09 +0300

    Update tokenlist.ts

--- src/lib/tokenlist.ts
+++ src/lib/tokenlist.ts
@@ -1,13 +1,15 @@
-{
-  "chainId": 101,
-  "address": "HjHgtmRntsg3wU4aED7PfQ6oFq4XbmMrLjmRAkG5LumE",
-  "symbol": "LUSD",
-  "name": "LumE USD",
-  "decimals": 8,
-  "logoURI": "https://gateway.pinata.cloud/ipfs/bafybeifqn76mz2ctuzludo6uv4v5wm76zjc6oinfg7ez2sqpuk5ig6rzaa",
-  "tags": ["stablecoin"],
-  "extensions": {
-    "website": "https://t.me/LumeUSD",
-    "description": "This token is built on Solana for fast and low-cost transactions."
-  }
-}
+import { TokenInfo } from '@solana/spl-token-registry';
+
+export const lumeToken: TokenInfo = {
+  chainId: 101, // 101 = Solana Mainnet
+  address: 'HjHgtmRntsg3wU4aED7PfQ6oFq4XbmMrLjmRAkG5LumE',
+  symbol: 'LUSD',
+  name: 'LumE USD',
+  decimals: 8,
+  logoURI: 'https://gateway.pinata.cloud/ipfs/bafybeifqn76mz2ctuzludo6uv4v5wm76zjc6oinfg7ez2sqpuk5ig6rzaa',
+  tags: ['stablecoin', 'usd-pegged'],
+  extensions: {
+    telegram: 'https://t.me/LumeUSD',
+    website: 'https://lumeusd.com', // если сайта пока нет, можно убрать эту строку
+  },
+};
