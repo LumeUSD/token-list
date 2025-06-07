7f733b6816b7129ae406b52b02bcafcb2f286f53
LumeUSD <eth.bitcoin05@gmail.com>
2025-06-07 03:34:24 +0300

    Create tokenlist.ts

---
+++ src/tokenlist.ts
@@ -0,0 +1,13 @@
+{ TokenInfo }
+  chainId: 101, // 101 = Solana Mainnet
+  address: 'HjHgtmRntsg3wU4aED7PfQ6oFq4XbmMrLjmRAkG5LumE',
+  symbol: 'LUSD',
+  name: 'LumE USD',
+  decimals: 8,
+  logoURI: 'https://gateway.pinata.cloud/ipfs/bafybeifqn76mz2ctuzludo6uv4v5wm76zjc6oinfg7ez2sqpuk5ig6rzaa',
+  tags: ['stablecoin', 'usd-pegged'],
+  extensions: {
+    telegram: 'https://t.me/LumeUSD',
+    website: 'https://lumeusd.com', //
+  },
+};
